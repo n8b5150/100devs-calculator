34b69bd692651d9a9725439070133739e1082005
n8b5150 <87131703+n8b5150@users.noreply.github.com>
2022-03-25 14:48:04 -0400

    created html, css, and js files, added elements, some styling, and event listeners with notes

---
+++ main.js
@@ -0,0 +1,32 @@
+//add event listeners to li elements
+const zero = document.getElementById('0').addEventListener('click',)
+const one = document.getElementById('1').addEventListener('click',)
+const two = document.getElementById('2').addEventListener('click',)
+const three = document.getElementById('3').addEventListener('click',)
+const four = document.getElementById('4').addEventListener('click',)
+const five = document.getElementById('5').addEventListener('click',)
+const six = document.getElementById('6').addEventListener('click',)
+const seven = document.getElementById('7').addEventListener('click',)
+const eight = document.getElementById('8').addEventListener('click',)
+const nine = document.getElementById('9').addEventListener('click',)
+const divide = document.getElementById('divide').addEventListener('click',)
+const multiply = document.getElementById('multiply').addEventListener('click',)
+const add = document.getElementById('add').addEventListener('click',)
+const subtract = document.getElementById('subtract').addEventListener('click',)
+const equals = document.getElementById('equals').addEventListener('click',)
+const decimal = document.getElementById('decimal').addEventListener('click',)
+
+//display - section selected to display current and total
+const display = document.getElementById('display').innerText
+
+//current - string to store numbers and operators
+let current = 0
+//total - string to display result of calculation
+let total = 0
+
+//listen for events and create 'current' string and display
+//async, loop waiting for each input?
+
+
+//when 'equals' is pressed, calculate 'total' and display
+
