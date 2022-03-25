ec7e64c3fab853295c4961922c87e43fd24dda16
n8b5150 <87131703+n8b5150@users.noreply.github.com>
2022-03-25 16:51:28 -0400

    added style and layout

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 //add event listeners to li elements
-const zero = document.getElementById('0').addEventListener('click',)
-const one = document.getElementById('1').addEventListener('click',)
-const two = document.getElementById('2').addEventListener('click',)
-const three = document.getElementById('3').addEventListener('click',)
-const four = document.getElementById('4').addEventListener('click',)
-const five = document.getElementById('5').addEventListener('click',)
-const six = document.getElementById('6').addEventListener('click',)
-const seven = document.getElementById('7').addEventListener('click',)
-const eight = document.getElementById('8').addEventListener('click',)
-const nine = document.getElementById('9').addEventListener('click',)
+const zero = document.getElementById('zero').addEventListener('click',zeroIn)
+const one = document.getElementById('one').addEventListener('click',)
+const two = document.getElementById('two').addEventListener('click',)
+const three = document.getElementById('three').addEventListener('click',)
+const four = document.getElementById('four').addEventListener('click',)
+const five = document.getElementById('five').addEventListener('click',)
+const six = document.getElementById('six').addEventListener('click',)
+const seven = document.getElementById('seven').addEventListener('click',)
+const eight = document.getElementById('eight').addEventListener('click',)
+const nine = document.getElementById('nine').addEventListener('click',)
 const divide = document.getElementById('divide').addEventListener('click',)
 const multiply = document.getElementById('multiply').addEventListener('click',)
 const add = document.getElementById('add').addEventListener('click',)
@@ -17,15 +17,24 @@ const equals = document.getElementById('equals').addEventListener('click',)
 const decimal = document.getElementById('decimal').addEventListener('click',)
 
 //display - section selected to display current and total
-const display = document.getElementById('display').innerText
+const display = document.getElementById('result').innerText
 
 //current - string to store numbers and operators
-let current = 0
+let current = ''
 //total - string to display result of calculation
-let total = 0
+let total = ''
 
 //listen for events and create 'current' string and display
 //async, loop waiting for each input?
+const zeroIn = () => {
+    current+='0'
+    result(current)
+}
+
+//display current value
+const result = (num) => {
+    display = num
+}
 
 
 //when 'equals' is pressed, calculate 'total' and display
