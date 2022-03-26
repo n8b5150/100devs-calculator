1b95a15b158b287f7d474c8608ae8fb70df510c4
Nathan Burks <87131703+n8b5150@users.noreply.github.com>
2022-03-25 21:33:51 -0400

    added remaining event listeners, added operators, confirmed operation. To do: add character limit.

--- main.js
+++ main.js
@@ -1,41 +1,132 @@
-//add event listeners to li elements
-const zero = document.getElementById('zero').addEventListener('click',zeroIn)
-const one = document.getElementById('one').addEventListener('click',)
-const two = document.getElementById('two').addEventListener('click',)
-const three = document.getElementById('three').addEventListener('click',)
-const four = document.getElementById('four').addEventListener('click',)
-const five = document.getElementById('five').addEventListener('click',)
-const six = document.getElementById('six').addEventListener('click',)
-const seven = document.getElementById('seven').addEventListener('click',)
-const eight = document.getElementById('eight').addEventListener('click',)
-const nine = document.getElementById('nine').addEventListener('click',)
-const divide = document.getElementById('divide').addEventListener('click',)
-const multiply = document.getElementById('multiply').addEventListener('click',)
-const add = document.getElementById('add').addEventListener('click',)
-const subtract = document.getElementById('subtract').addEventListener('click',)
-const equals = document.getElementById('equals').addEventListener('click',)
-const decimal = document.getElementById('decimal').addEventListener('click',)
+
 
 //display - section selected to display current and total
-const display = document.getElementById('result').innerText
+const display = document.getElementById('result')
 
 //current - string to store numbers and operators
 let current = ''
 //total - string to display result of calculation
 let total = ''
-
-//listen for events and create 'current' string and display
-//async, loop waiting for each input?
-const zeroIn = () => {
-    current+='0'
-    result(current)
-}
+let currentArr = []
 
 //display current value
-const result = (num) => {
-    display = num
+const result = (str) => {
+    display.innerText = str
 }
 
 
 //when 'equals' is pressed, calculate 'total' and display
+const equals = () => {
+    currentArr = current.split(' ')
+    console.table(currentArr)
+    current = eval(current)
+    result(current)
+}
 
+
+//listen for events and create 'current' string and display
+//async, loop waiting for each input?
+const zero = () => {
+    if (current.length>=1){
+        current+='0'
+        result(current)
+    }
+}
+const one = () => {
+    current+='1'
+    result(current)
+}
+const two = () => {
+    current+='2'
+    result(current)
+}
+const three = () => {
+    current+='3'
+    result(current)
+}
+const four = () => {
+    current+='4'
+    result(current)
+}
+const five = () => {
+    current+='5'
+    result(current)
+}
+const six = () => {
+    current+='6'
+    result(current)
+}
+const seven = () => {
+    current+='7'
+    result(current)
+}
+const eight = () => {
+    current+='8'
+    result(current)
+}
+const nine = () => {
+    current+='9'
+    result(current)
+}
+const divide = () => {
+    if (current[current.length -1 ] == ' '){
+        current = current.slice(0,current.length-3)
+        current+=' / '
+        result(current)
+    } else {
+        current+=' / '
+        result(current)
+    }
+}
+const multiply = () => {
+     if (current[current.length -1 ] == ' '){
+        current = current.slice(0,current.length-3)
+        current+=' * '
+        result(current)
+    } else {
+        current+=' * '
+        result(current)
+    }
+}
+const add = () => {
+     if (current[current.length -1 ] == ' '){
+        current = current.slice(0,current.length-3)
+        current+=' + '
+        result(current)
+    } else {
+        current+=' + '
+        result(current)
+    }
+}
+const subtract = () => {
+     if (current[current.length -1 ] == ' '){
+        current = current.slice(0,current.length-3)
+        current+=' - '
+        result(current)
+    } else {
+        current+=' - '
+        result(current)
+    }
+}
+const decimal = () => {
+    current+='.'
+    result(current)
+}
+
+//add event listeners to li elements
+document.getElementById('zero').addEventListener('click',zero)
+document.getElementById('one').addEventListener('click',one)
+document.getElementById('two').addEventListener('click',two)
+document.getElementById('three').addEventListener('click',three)
+document.getElementById('four').addEventListener('click',four)
+document.getElementById('five').addEventListener('click',five)
+document.getElementById('six').addEventListener('click',six)
+document.getElementById('seven').addEventListener('click',seven)
+document.getElementById('eight').addEventListener('click',eight)
+document.getElementById('nine').addEventListener('click',nine)
+document.getElementById('divide').addEventListener('click',divide)
+document.getElementById('multiply').addEventListener('click',multiply)
+document.getElementById('add').addEventListener('click',add)
+document.getElementById('subtract').addEventListener('click',subtract)
+document.getElementById('equals').addEventListener('click',equals)
+document.getElementById('decimal').addEventListener('click',decimal)
